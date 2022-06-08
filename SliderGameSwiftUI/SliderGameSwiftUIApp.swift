b25bd5e294d552f7fc7970f397a39ca2cec4cf0a
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-06-08 12:03:16 +0300

    Initial Commit

---
+++ SliderGameSwiftUI/SliderGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameSwiftUIApp.swift
+//  SliderGameSwiftUI
+//
+//  Created by Василий Полторак on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
